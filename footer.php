3cc3832cfefd0c1427873d6f3a5834d9dbcbbb5d
Tristan <tristan@fddl.dev>
2026-05-14 06:53:45 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,8 +4,9 @@
             <!-- Widgets would go here -->
         </div>
         <div class="footer-bottom">
-            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. By musicians, for musicians.</p>
-            <p>Built using <a href="https://wordpress.org" target="_blank">WordPress</a>. Developted by <a href="https://fddl.dev" target="_blank">Tristan</a>. </p>
+            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. By musicians, for musicians.</p><br />
+            <p>Built using <a href="https://wordpress.org" target="_blank">WordPress</a>. Developed by <a href="https://fddl.dev" target="_blank">Tristan</a>.</p><br />
+            <p>The dream of <a href="https://bethanyrhodes.com" target="_blank">Bethany</a>.</p>
         </div>
     </div>
 </footer>
